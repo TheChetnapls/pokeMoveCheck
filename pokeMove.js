f05624e19d4498701861f34df1c12e41d42d1ee6
TheChetnapls <36456949+TheChetnapls@users.noreply.github.com>
2021-07-12 11:24:25 -0700

    Cleaned up loops

--- pokeMove.js
+++ pokeMove.js
@@ -22,48 +22,38 @@ axios.get(`https://pokeapi.co/api/v2/pokemon-species/${pokeName}/`)
                         pokeEvol.push(response.data.chain.evolves_to[0].evolves_to[0].species.name);
                     }
                 }
-                console.log(pokeEvol);
-                console.log('0')
                 return pokeEvol;
             })
-            .catch(error => console.log(error))
-        console.log(pokeEvol);
-        console.log('1')
-        return pokeEvol;
-    })
-    .then(pokeEvol => {
-        console.log(pokeEvol);
-        console.log('2');
-        for (let i = 0; i < pokeEvol.length; i++) {
-            axios.get(`https://pokeapi.co/api/v2/pokemon/${pokeEvol[i]}/`)
-                .then(response => {
-                    console.log(pokeEvol);
-                    console.log('3');
-
-                    let moves = response.data.moves;
-                    //check all moves to see if pokeMove can be learned
-                    for (let i = 0; i < moves.length; i++) {
-                        //check to see if it learns it by leveling
-                        let moveLevel = moves[i].version_group_details[0].level_learned_at;
-                        if (pokeMove == moves[i].move.name && moveLevel > 0) {
-                            if (pokeName == pokeEvol[i]) {
-                                console.log(`${pokeName} learns ${pokeMove} at level ${moveLevel}.`);
-                            } else if (pokeName == pokeEvol[0] && pokeEvol[i] == pokeEvol[1]) {
-                                console.log(`${pokeName}'s next evolution, ${pokeEvol[i]} can learn ${pokeMove} at level ${moveLevel}.`);
-                            } else if (pokeName == pokeEvol[0] && pokeEvol[i] == pokeEvol[2]) {
-                                console.log(`${pokeName}'s last evolution, ${pokeEvol[i]} can learn ${pokeMove} at level ${moveLevel}.`);
-                            } else if (pokeName == pokeEvol[1] && pokeEvol[i] == pokeEvol[0]) {
-                                console.log(`${pokeName}'s previous evolution, ${pokeEvol[i]} can learn ${pokeMove} at level ${moveLevel}.`);
-                            } else if (pokeName == pokeEvol[1] && pokeEvol[i] == pokeEvol[2]) {
-                                console.log(`${pokeName}'s next evolution, ${pokeEvol[i]} can learn ${pokeMove} at level ${moveLevel}.`);
-                            } else if (pokeName == pokeEvol[2] && pokeEvol[i] == pokeEvol[0]) {
-                                console.log(`${pokeName}'s starting evolution, ${pokeEvol[i]} can learn ${pokeMove} at level ${moveLevel}.`);
-                            } else if (pokeName == pokeEvol[2] && pokeEvol[i] == pokeEvol[1]) {
-                                console.log(`${pokeName}'s previous evolution, ${pokeEvol[i]} can learn ${pokeMove} at level ${moveLevel}.`);
-                            }
-                        }
+            .then(pokeEvol => {
+                let foundCurrentPoke = false;
+                let foundMove = false;
+                pokeEvol.forEach(pokeEvol => {
+                    //check if you're on the pokemon user searched for, or if you already passed it
+                    //and couldn't find the move
+                    if (pokeEvol == pokeName || !foundMove) {
+                        foundCurrentPoke = true;
                     }
+                    if (!foundCurrentPoke) return;
+                    axios.get(`https://pokeapi.co/api/v2/pokemon/${pokeEvol}/`)
+                        .then(response => {
+                            let moves = response.data.moves;
+                            moves.forEach(move => {
+                                //returns if pokemon move doesn't match, or if move is already found
+                                if (pokeMove != move.move.name || foundMove) return;
+                                move.version_group_details.forEach(version => {
+                                    if (version.level_learned_at == 0 || version.level_learned_at == 1 || foundMove) return;
+                                    console.log(`${pokeEvol} learns ${pokeMove} at level ${version.level_learned_at}`);
+                                    foundMove = true;
+                                })
+                            })
+                        })
+                        .catch(error => console.log(error));
                 })
-        }
+                //in case no one learns the move
+                if (!foundMove) {
+                    console.log(`${pokeName} does not learn ${pokeMove}`);
+                }
+            })
+            .catch(error => console.log(error));
     })
-    .catch(error => console.log(error))
+    .catch(error => console.log(error));
